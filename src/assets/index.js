d087bae224bd25915946a64316c17e7e077a52c8
Marvin <marvinshawn9333@gmail.com>
2018-01-26 23:32:02 +0800

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+import category from "./category.svg";
+import profile from "./profile.svg";
+import shopping_cart from "./shoppingCart.svg";
+import arrow_right from "./arrowRight.svg";
+
+export { category, profile, shopping_cart, arrow_right };
